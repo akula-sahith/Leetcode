f9b06c3db3c789d0f0009d0558fc1d3f8ecf8b94
akula-sahith <145775406+akula-sahith@users.noreply.github.com>
2026-03-01 21:20:23 +0000

    Sync LeetCode submissions

---
+++ Java/minimum-number-of-days-to-make-m-bouquets/Solution.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+
+        if((long)m*k > bloomDay.length){
+            return -1;
+        }
+
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MIN_VALUE;
+        for(int day : bloomDay){
+            left = Math.min(left , day);
+            right = Math.max(right , day);
+        }
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(canMake(bloomDay,m,k,mid)){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public boolean canMake(int[] bloomDay,int m,int k,int days){
+        int boques_made = 0;
+        int each_boque = k;
+        int last_used = 0;
+        boolean fresh_boque = true;
+        for(int i = 0;i<bloomDay.length;i++){
+            if(bloomDay[i] <= days){
+              if(fresh_boque){
+                each_boque--;
+                fresh_boque = false;
+                last_used = i;
+              }else{
+                if((i - 1)==last_used){
+                    each_boque--;
+                    last_used = i;
+                }
+              }
+              
+              if(each_boque == 0){
+                boques_made++;
+                fresh_boque = true;
+                each_boque = k;
+              }
+            }else{
+                each_boque = k;
+                fresh_boque = true;
+            }
+        }
+
+        return boques_made >= m;
+    }
+}
