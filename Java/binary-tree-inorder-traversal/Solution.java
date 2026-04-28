2596418c4e6d1edd6461947e6ff323042f481283
akula-sahith <145775406+akula-sahith@users.noreply.github.com>
2026-04-28 11:02:36 +0000

    Sync LeetCode submissions

--- Java/binary-tree-inorder-traversal/Solution.java
+++ Java/binary-tree-inorder-traversal/Solution.java
@@ -15,7 +15,8 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        return inorder(root,new ArrayList<Integer>());
+        // return inorder(root,new ArrayList<Integer>());
+         return inorder2(root);
     }
 
     //Helper function for Inorder Traversal
@@ -28,4 +29,25 @@ class Solution {
         list = inorder(node.right,list);
         return list;
     }
+    
+    //Stack based approach for the inorder traversal
+    public List<Integer> inorder2(TreeNode node){
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        // stack.push(node);
+        TreeNode curr = node;
+        while(curr!=null || !stack.isEmpty()){
+            while(curr!=null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            ans.add(curr.val);
+            curr = curr.right;
+        }
+
+        return ans;
+    }
+    
 }
