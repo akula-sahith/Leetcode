9b2310eaa29b7bbf97aaf72f7df306cbcdbc8a0c
akula-sahith <145775406+akula-sahith@users.noreply.github.com>
2026-05-03 21:38:40 +0000

    Sync LeetCode submissions

--- Java/kth-largest-element-in-an-array/Solution.java
+++ Java/kth-largest-element-in-an-array/Solution.java
@@ -1,16 +1,30 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
-        for(int i = 0;i<nums.length;i++){
-            queue.add(nums[i]);
+    // PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+    //     for(int i = 0;i<nums.length;i++){
+    //         queue.add(nums[i]);
+    //     }
+
+    //     while(k > 1){
+    //        int ele = queue.poll();
+    //        System.out.println(ele);
+    //        k--;
+    //     }
+
+    //     return queue.poll();
+    return approach2(nums,k);
+    }
+
+    public int approach2(int[] nums,int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i = 0 ; i < nums.length ; i++){
+            pq.offer(nums[i]);
+            if(pq.size() > k){
+                pq.poll();
+            }
         }
 
-        while(k > 1){
-           int ele = queue.poll();
-           System.out.println(ele);
-           k--;
-        }
-
-        return queue.poll();
+        return pq.poll();
     }
 }
--- Java/number-of-provinces/Solution.java
+++ Java/number-of-provinces/Solution.java
@@ -6,7 +6,7 @@ class Solution {
 
         for(int i = 0 ; i < n ; i++){
             if(!visited[i]){
-                dfs(isConnected,visited,i);
+                bfs(isConnected,visited,i);
                 provinces++;
             }
         }
@@ -19,6 +19,23 @@ class Solution {
         for(int j = 0 ; j < graph.length ; j++){
             if(graph[city][j]==1 && !visited[j]){
                 dfs(graph,visited,j);
+            }
+        }
+        return;
+    }
+
+    public void bfs(int[][] graph,boolean[] visited,int city){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(city);
+        visited[city] = true;
+        while(!queue.isEmpty()){
+            int polled = queue.poll();
+
+            for(int j = 0 ; j < graph.length ; j++){
+                if(graph[polled][j]==1 && !visited[j]){
+                    visited[j] = true;
+                    queue.offer(j);
+                }
             }
         }
         return;
